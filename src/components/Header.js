e02300ef6c0637cb41b27d734ddf21a2b06a0465
Tramy Nguyen <tramynn2@gmail.com>
2019-09-02 18:25:09 -0500

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
-import './Header.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const searchFavicon = <FontAwesomeIcon icon={faSearch} color="#37284C" />
+const searchFavicon = <FontAwesomeIcon icon={faSearch} color="#37284C" />;
 
 class Header extends Component {
-    render() {
-        return(
-            <header className="NavBar">
-                <nav className="NavBar-Left">
-                    <section className="NavBar-Logo">
-                        <h1>reSolv</h1>
-                    </section>
-                </nav>
-                <nav className="Navbar-Right">
-                    <section className="NavBar-Links">
-                        <ul>
-                            <li onClick={ () =>this.props.updateDisplay("Home")}>Home</li>
-                            <li onClick={ () =>this.props.updateDisplay("Quiz")}>Take Quiz</li>
-                            <li onClick={ () =>this.props.updateDisplay("ConflictStyles")}>Conflict Styles</li>
-                            <li onClick={ () =>this.props.updateDisplay("ConflictTypes")}>Conflict Types</li>
-                            <li onClick={ () =>this.props.updateDisplay("Results")}>Results</li>
-                            <li>
-                                <form className="Search-Input" >
-                                    <input type="text" placeholder="Search.." />
-                                    <button className="Search-Button">
-                                    {searchFavicon}
-                                    </button>
-                                </form>
-                            </li>
-                        </ul>
-                    </section>
-                </nav>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="NavBar">
+        <nav className="NavBar-Left">
+          <section className="NavBar-Logo">
+            <h1>reSolv</h1>
+          </section>
+        </nav>
+        <nav className="Navbar-Right">
+          <section className="NavBar-Links">
+            <ul>
+              <li onClick={() => this.props.updateDisplay("Home")}>Home</li>
+              <li onClick={() => this.props.updateDisplay("Quiz")}>
+                Take Quiz
+              </li>
+              <li onClick={() => this.props.updateDisplay("ConflictStyles")}>
+                Conflict Styles
+              </li>
+              <li onClick={() => this.props.updateDisplay("ConflictTypes")}>
+                Conflict Types
+              </li>
+              <li>
+                <form className="Search-Input">
+                  <input type="text" placeholder="Search.." />
+                  <button className="Search-Button">{searchFavicon}</button>
+                </form>
+              </li>
+            </ul>
+          </section>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
