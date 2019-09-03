d4984a7fc30e4babc9f11a4f8483d4fbb4137405
Tramy Nguyen <tramynn2@gmail.com>
2019-09-02 21:50:59 -0500

    added gradient to quiz decription container

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
-import './Home.css';
-import Footer from '../Footer/Footer';
+import React, { Component } from "react";
+import "./Home.css";
+import Footer from "../Footer/Footer";
 
 class Home extends Component {
-    render() {
-        return(
-            <section className="Home">
-                <div className="Home-Container">
-                    <h1 className="Home-Title">Transform your conflicts</h1>
-                    <p className="Home-Text">Learn about your conflict style and improve your communication skills.</p>
-                    <button className="Take-Quiz-Button" onClick={() => this.props.updateDisplay("Quiz")}>Take Quiz</button>
-                    <div className="Home-Footer">
-                        <Footer />
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="Home">
+        <div className="Home-Container">
+          <h1 className="Home-Title">Transform your conflicts</h1>
+          <p className="Home-Text">
+            Learn about your conflict style and improve your communication
+            skills.
+          </p>
+          <button
+            className="Take-Quiz-Button"
+            onClick={() => this.props.updateDisplay("Quiz")}
+          >
+            Take Quiz
+          </button>
+          <div className="Home-Footer">
+            <Footer />
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Home;
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,25 +7,15 @@ class Quiz extends Component {
   render() {
     return (
       <section className="Quiz">
-        <h1 className="Quiz-Title">Conflict Style Quiz</h1>
         <div className="Quiz-Description-Container">
+          <h1 className="Quiz-Title">Conflict Style Quiz</h1>
           <section className="Quiz-Description">
-            Powder cookie apple pie jelly-o tootsie roll apple pie chocolate
-            cake. Jelly gummies gingerbread chocolate cake caramels dessert tart
-            icing. Dragée chocolate bar candy topping. Topping liquorice carrot
-            cake powder powder dragée. Liquorice marshmallow chocolate. Cotton
-            candy gummi bears pie sugar plum chocolate bar toffee. Liquorice
-            muffin gingerbread fruitcake gingerbread ice cream icing jelly-o
-            dragée. Chocolate cake powder cake pastry chocolate bar dessert
-            chocolate bar. Candy canes sweet roll macaroon bonbon sweet roll
-            dragée sugar plum sugar plum caramels. Halvah chocolate brownie. Ice
-            cream jelly beans fruitcake dragée lollipop jelly-o cotton candy
-            tootsie roll marzipan. Chocolate cake donut dessert soufflé
-            liquorice macaroon danish sesame snaps gingerbread. Marshmallow
-            sesame snaps marzipan croissant cheesecake. Toffee chocolate cake
-            brownie.
+            <section className=""></section>
+            <section className=""></section>
+            <section className=""></section>
           </section>
         </div>
+        {/* <div className="Quiz-Gradient"></div> */}
         <Card updateDisplay={this.props.updateDisplay} />
         <Footer />
       </section>
