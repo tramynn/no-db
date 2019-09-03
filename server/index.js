61407fdd337f30e896863330c98eb15e610b265d
Tramy Nguyen <tramynn2@gmail.com>
2019-09-02 19:26:51 -0500

    changed favicon

--- server/index.js
+++ server/index.js
@@ -7,10 +7,8 @@ app.use(express.json());
 app.get("/api/quiz", quizController.getQuizQuestions);
 app.post("/api/quiz", quizController.addAnswers);
 app.get("/api/results", quizController.getResults);
-app.put("/api/quiz/:name", quizController.updateAnswers)
+app.put("/api/quiz/:name", quizController.updateAnswers);
 app.delete("/api/quiz", quizController.deleteAnswers);
 
 const PORT = 5225;
-app.listen(PORT, 
-    () => console.log(`Listening on PORT: ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
