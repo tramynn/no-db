6cf777ca3d4cf3dceef0fd555e34c63020022c00
Tramy Nguyen <tramynn2@gmail.com>
2019-09-03 03:24:40 -0500

    styled sn nav

--- src/components/ConflictStyles/StylesNav/StylesNav.js
+++ src/components/ConflictStyles/StylesNav/StylesNav.js
@@ -1,30 +1,55 @@
-import React, { Component } from 'react';
-import './StylesNav.css';
+import React, { Component } from "react";
+import "./StylesNav.css";
 
 class StylesNav extends Component {
-    render() {
-        return(
-            <nav className="StylesNav">
-                <ul>
-                    <button className="Collab-button">
-                        <li onClick={ () => this.props.updateSection("Collaborating")}>Collaborating</li>
-                    </button>
-                    <button>
-                        <li onClick={ () => this.props.updateSection("Competing")}>Competing</li>
-                    </button>
-                    <button>
-                        <li onClick={ () => this.props.updateSection("Avoiding")}>Avoiding</li>
-                    </button>
-                    <button>
-                        <li onClick={ () => this.props.updateSection("Accommodating")}>Accommodating</li>
-                    </button>
-                    <button>
-                        <li onClick={ () => this.props.updateSection("Compromising")}>Compromising</li>
-                    </button>
-                </ul>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="StylesNav">
+        <ul>
+          <button className="SN">
+            <li
+              className="SN-Link"
+              onClick={() => this.props.updateSection("Collaborating")}
+            >
+              Collaborating
+            </li>
+          </button>
+          <button className="SN">
+            <li
+              className="SN-Link"
+              onClick={() => this.props.updateSection("Competing")}
+            >
+              Competing
+            </li>
+          </button>
+          <button className="SN">
+            <li
+              className="SN-Link"
+              onClick={() => this.props.updateSection("Avoiding")}
+            >
+              Avoiding
+            </li>
+          </button>
+          <button className="SN">
+            <li
+              className="SN-Link"
+              onClick={() => this.props.updateSection("Accommodating")}
+            >
+              Accommodating
+            </li>
+          </button>
+          <button className="SN">
+            <li
+              className="SN-Link"
+              onClick={() => this.props.updateSection("Compromising")}
+            >
+              Compromising
+            </li>
+          </button>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default StylesNav;
